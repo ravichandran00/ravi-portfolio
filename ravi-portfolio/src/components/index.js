62cc9f905d7cb675c3218ad00974e926239dc77a
ravichandran00 <94168982+ravichandran00@users.noreply.github.com>
2024-01-29 01:10:49 +0530

    Delete ravi-portfolio directory

--- ravi-portfolio/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Navbar } from './Navbar/NavBar';
-export { default as HeroSection } from './Hero/HeroSection';
-export { default as SkillsSection } from './Skills/SkillsSection';
-export { default as Experience } from './WorkExperience/Experience';
-export { default as FooterSection } from './Footer/FooterSection';
